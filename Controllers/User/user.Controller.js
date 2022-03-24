cd4449604babe3da97d0d1b79850881d96b6d135
5-VED <parmarvedpro5@gmail.com>
2022-03-24 23:26:19 +0530

    Created update User API for user

--- Controllers/User/user.Controller.js
+++ Controllers/User/user.Controller.js
@@ -2,6 +2,7 @@ const User = require('../../Models/user');
 const bcrypt = require('bcrypt');
 
 exports.signup = async (req, res, next) => {
+
     try {
         const user = await User.create({
             firstName: req.body.firstName,
@@ -18,15 +19,9 @@ exports.signup = async (req, res, next) => {
             console.log(user);
             return res.status(200).json({ message: "User was registered Succesfully" });
         }
-        else {
-            return res.status(500).json({ message: err });
-        }
-
     } catch (err) {
-        console.log('error====> ', err);
         return res.status(500).json({ message: err });
     }
-
 }
 
 
@@ -34,18 +29,21 @@ exports.signin = {
 
 }
 
+
+
+
 exports.getUser = async (req, res) => {
+    const user = await User.findOne({ _id: req.params.id });
     try {
-        const user = await User.findOne({_id:req.params.id});
-        if(!user){
-            res.status(500).json({message:"User not Found"});
-        }        
-        else{
+        if (!user) {
+            res.status(500).json({ message: "User not Found" });
+        }
+        else {
             res.status(200).send(user);
         }
-     } catch (err) { 
-         res.send(500).send(err)
-     }
+    } catch (err) {
+        res.send(500).send(err)
+    }
 }
 
 exports.getAllUsers = async (req, res) => {
@@ -56,3 +54,45 @@ exports.getAllUsers = async (req, res) => {
         res.status(500).json({ message: err })
     }
 }
+
+exports.deleteUser = async (req, res) => {
+    const user = await User.findOne({ _id: req.params.id })
+    try {
+        if (user.isDeleted === true || !user) {
+            res.status(500).json({ message: "User Does not Exist" });
+        } else {
+            user.isDeleted = true
+            await user.save();
+            res.status(200).json({ message: "User Deleted Succesfully" });
+        }
+    } catch (err) {
+        res.status(500).json({ message: "Some Error Occured while deleting the User" });
+    }
+
+}
+
+exports.updateUser = (req, res) => {
+    console.log("over here ins");
+    User.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            $set: {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                userName: req.body.userName,
+                email: req.body.email,
+                password: req.body.password,
+                contactNumber: req.body.contactNumber,
+            }
+        }, (err, result) => {
+            if (err) {
+                return res.status(500).json({ msg: "Some Error Occured" })
+            }
+            else {
+                console.log("over here");
+                return res.status(200).json({ Message: "Data updated succesfully", user: req.body })
+            }
+        })
+
+}
+
--- Routes/User/user.route.js
+++ Routes/User/user.route.js
@@ -2,13 +2,14 @@ const express = require('express');
 const userController = require('../../Controllers/User/user.Controller');
 const router = express.Router();
 
+router.post('/signup', userController.signup);
 
-router.post('/signup',userController.signup);
+router.get('/allUsers', userController.getAllUsers);
 
-router.get('/allUsers',userController.getAllUsers);
+router.get('/user/:id', userController.getUser);
 
-router.get('/user',userController.getUser);
+router.put('/user/:id', userController.deleteUser);
 
-
+router.put('/updateUser/:id', userController.updateUser);
 
 module.exports = router;
